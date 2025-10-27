13d62480500ea2076e21be796e633f662147e098
Álvaro <alvaroogarcia97@gmail.com>
2025-10-27 12:29:45 +0100

    metiendo estilos y conidciones

--- app/index.php
+++ app/index.php
@@ -1,8 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+<link rel="stylesheet" href="estilos.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo.css">
     <title>Document</title>
 </head>
 <body>
@@ -12,8 +14,8 @@
     </header>
     <main>
         <section id="contenedor_frm" name="contenedor_frm">
-            <form id="frm_datos" action="datos.php"></form>
-            <fieldset>
+            <form id="frm_datos" action="datos.php">
+                 <fieldset>
                 <legend>Datos usarios</legend>
                 <article>
                     <label for="nombre">Nombre</label>
@@ -35,6 +37,8 @@
                 <article>
                     <label for="password">Contraseña</label>
                     <input type="password" name="password" id="password" placeholder="Contraseña Usuario" required >
+                     <label for="password">Confirmar Contraseña</label>
+                    <input type="password" name="confirmarPassword" id="password" placeholder="Confirmar Contraseña" required >
                 </article>
 
                   <article>
@@ -67,6 +71,8 @@
                     
                 </article>
             </fieldset>
+            </form>
+           
         </section>
         
     </main>
@@ -75,3 +81,51 @@
     </footer>   
 </body>
 </html>
+
+<?php
+
+// variables vacias
+$nombre = "";
+$apellidos = "";
+$password = "";
+$confirmarPassword = "";
+$error_nombre = "";
+$error_password = "";
+$pais = "";
+$fechaN = "";
+$error_apellidos = "";
+$error_pais = "";
+$error_fecha = "";
+
+if($_SERVER["REQUEST_METHOD"]== "POST"){
+    $nombre = trim($_POST['nombre']);
+    $apellidos = trim($_POST['apellidos']);
+    $password = trim($_POST['password']);
+    $confirmarPassword = trim($_POST['confirmarPassword']);
+    $pais = trim($_POST['pais']);
+    $fechaN = trim($_POST['fecha']);
+    
+
+
+    // Validación de campos
+    if (empty($nombre)) {
+        $error_nombre = "El nombre está vacío";
+    }
+    if (empty($apellidos)) {
+        $error_apellidos = "Los apellidos están vacíos";
+    }
+    if (empty($password)) {
+        $error_password = "La contraseña está vacía";
+    }
+    if (empty($pais)) {
+        $error_pais = "Debe seleccionar un país";
+    }
+    if (empty($fechaN)) {
+        $error_fecha = "Debe seleccionar la fecha de nacimiento";
+    }
+
+}
+
+ 
+
+
