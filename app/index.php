f7aa89efe9de177d147844dfa26155ffaf310c37
AlvaroGarcia97 <alvaroogarcia97@gmail.com>
2025-10-30 22:51:47 +0100

    arreglado

--- app/index.php
+++ app/index.php
@@ -1,10 +1,84 @@
+<?php
+
+// variables vacias
+// Así evitanmos errores y puedes mostrar los valores al recargar el formulario.
+$nombre = "";
+$apellidos = "";
+$password = "";
+$email = "";
+$confirmarPassword = "";
+$pais = "";
+$fecha = "";
+$acepto = false;
+$error = [];
+
+if($_SERVER["REQUEST_METHOD"]== "POST"){
+    $nombre = trim($_POST['nombre'] ?? "");
+    $apellidos = trim($_POST['apellidos'] ?? "");
+    $password = trim($_POST['password'] ?? "");
+    $confirmarPassword = $_POST['confirmarPassword'] ?? "";
+    $email = trim($_POST['email'] ?? "");
+    $pais = trim($_POST['pais'] ?? "");
+    $fecha = trim($_POST['fecha'] ?? "");
+    $acepto = isset($_POST['acepto']) ? true : false;
+    
+
+
+    // Validación de campos
+    if (empty($nombre)) {
+       $error['nombre'] = "El nombre es obligatorio"; 
+    }
+    if (empty($apellidos)) {
+       $error['apellidos'] = "Los apellidos son obligatorios";
+    }
+    // Contraseña requerida y longitud mínima
+    if (empty($password)) {
+         $error['password'] = "La contraseña no puede estar vacía!!!"; 
+    }elseif(mb_strlen($password) < 8){
+         $error['password'] = "La contraseña no puede tener menos de 8 carácteres!!!"; 
+    }
+     // Confirmación de contraseña requerida y debe coincidir con la anterior
+     if ($confirmarPassword === ''){
+        $error['confirmarPassword'] = "Confirma tu contraseña";
+     } elseif ($password !== $confirmarPassword){
+        $error['confirmarPassword'] = "Las contraseñas no coinciden";
+     }
+    if (empty($pais)) {
+          $error['pais'] = "Selecciona el país porfa!!!"; 
+    }
+    if (empty($fecha)) {
+      $error['fecha'] = "SLa fecha es obligatoria!!!"; 
+    }
+     // Email requerido y con formato correcto
+    if(empty($email)){
+        $error['email'] = "El email no puede estar vacía!!!";
+    }elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)){
+        $error['email'] = "El correo no tiene el formato requerido!!!" ;
+    }
+    
+    if (!($acepto)){
+        $error['acepto'] = "Error debes aceptar las condiciones";
+    }
+    // Si no hay errores (el array está vacío), mostramos mensaje de éxito
+    if(empty($error)){
+         echo "<div>¡Registro completado con éxito!</div>";
+          echo "<p>Bienvenido/a $nombre $apellidos</p>";
+
+        exit(); // Detiene la ejecución para que no se muestre el formulario de nuevo
+    }
+
+
+
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Document</title>
+    <title>Formulario</title>
 </head>
 <body>
        <header>
@@ -13,17 +87,20 @@
     </header>
     <main>
         <section id="contenedor_frm" name="contenedor_frm">
-            <form id="frm_datos" action="datos.php">
+           <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+
                  <fieldset>
-                <legend>Datos usarios</legend>
+                <legend>Datos usuarios</legend>
                 <article>
                     <label for="nombre">Nombre</label>
                     <input type="text" name="nombre" id="nombre" placeholder="Nombre usuario" required>
+                     <span style="color:red;"><?php echo $error['nombre'] ?? ''; ?></span><br><br>
                 </article>
 
                   <article>
                     <label for="Apellidos">Apellidos</label>
-                    <input type="text" name="Apellidos" id="Apellidos" placeholder="Apellidos usuario" required>
+                    <input type="text" name="apellidos" id="apellidos" placeholder="Apellidos usuario" required>
+                     <span style="color:red;"><?php echo $error['apellidos'] ?? ''; ?></span><br><br>
                 </article>
 
                 
@@ -31,18 +108,23 @@
                 <article>
                     <label for="email">Correo</label>
                     <input type="email" name="email" id="email" placeholder="Correo Electrónico Usuario" required >
+                     <span style="color:red;"><?php echo $error['email'] ?? ''; ?></span><br><br>
                 </article>
 
                 <article>
                     <label for="password">Contraseña</label>
                     <input type="password" name="password" id="password" placeholder="Contraseña Usuario" required >
-                     <label for="password">Confirmar Contraseña</label>
-                    <input type="password" name="confirmarPassword" id="password" placeholder="Confirmar Contraseña" required >
+                    <span style="color:red;"><?php echo $error['password'] ?? ''; ?></span><br><br>
+                     <label for="confirmarPassword">Confirmar Contraseña</label>
+                    <input type="password" name="confirmarPassword" id="confirmarPassword" placeholder="Confirmar Contraseña" required >
+                    <span style="color:red;"><?php echo $error['confirmarPassword'] ?? ''; ?></span><br><br>
+
                 </article>
 
                   <article>
                     <label for="fecha">Fecha Nacimiento</label>
                     <input type="date" name="fecha" id="fecha"  required >
+                    <span style="color:red;"><?php echo $error['fecha'] ?? ''; ?></span>
                 </article>
 
                 <article>
@@ -54,10 +136,12 @@
                             <option value="PT">Holanda</option>
                             <option value="IT">Inglaterra</option>
                     </select>
+                    <span style="color:red;"><?php echo $error['pais'] ?? ''; ?></span><br><br>
 
                     <article>
                         <label for="acepto">Acepto los términos y condiciones:</label>
                         <input type="checkbox" id="acepto" name="acepto" required>
+                         <span style="color:red;"><?php echo $error['acepto'] ?? ''; ?></span><br><br>
                     </article>
                 </article>
 
@@ -81,60 +165,6 @@
 </body>
 </html>
 
-<?php
-
-// variables vacias
-$nombre = "";
-$apellidos = "";
-$password = "";
-$email = "";
-$confirmarPassword = "";
-$error_nombre = "";
-$error_password = "";
-$error_email = "";
-$pais = "";
-$fechaN = "";
-$error_apellidos = "";
-$error_pais = "";
-$error_fecha = "";
-
-if($_SERVER["REQUEST_METHOD"]== "POST"){
-    $nombre = trim($_POST['nombre']);
-    $apellidos = trim($_POST['apellidos']);
-    $password = trim($_POST['password']);
-    $confirmarPassword = trim($_POST['confirmarPassword']);
-    $pais = trim($_POST['pais']);
-    $fechaN = trim($_POST['fecha']);
-    
-
-
-    // Validación de campos
-    if (empty($nombre)) {
-        $error_nombre = "El nombre está vacío";
-    }
-    if (empty($apellidos)) {
-        $error_apellidos = "Los apellidos están vacíos";
-    }
-    if (empty($password)) {
-        $error_password = "La contraseña está vacía";
-    }elseif(mb_strlen($password) < 8){
-        $error_password = "la contraseña no tiene 8 carácteres";
-    }
-    if (empty($pais)) {
-        $error_pais = "Debe seleccionar un país";
-    }
-    if (empty($fechaN)) {
-        $error_fecha = "Debe seleccionar la fecha de nacimiento";
-    }
-    if(empty($email)){
-        $error_email = "el email no puede estar vacío";
-    }elseif(!filter_var($email,FILTER_VALIDATE_EMAIL)){
-        $error_email = "el codigo no tiene el formato que pedimos.";
-    } 
-
-
-
-}
 
  
 
